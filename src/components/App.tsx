5f531144f075868b6ea3194f71db97b028885c2d
Pavel Shvarchov <pavel@Pavels-iMac.lan>
2019-09-18 11:16:54 +0300

    added basic electron react webpack boilerplate

---
+++ src/components/App.tsx
@@ -0,0 +1,19 @@
+import '../assets/css/App.css'
+import React, { Component } from 'react'
+
+import url from "../assets/images/electron-react-webpack-boilerplate.png"
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, Electron!</h1>
+
+        <p>I hope you enjoy using basic-electron-react-boilerplate to start your dev off right!</p>
+
+        <img src={url} width="100%"/>
+      </div>
+    )
+  }
+}
+
+export default App
